602672eac512e4886e197ff1f0c7441abc2e28c3
Michael Hirabayashi <mthirabayashi@gmail.com>
2016-10-29 22:17:06 -0700

    add heroku setup

---
+++ frontend/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+});
