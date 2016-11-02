62e1164f92fb5f75316b9f919d5c5d50344b65d1
Michael Hirabayashi <mikehirabayashi@Michaels-MacBook-Air-2.local>
2016-11-01 18:10:16 -0700

    start css styling for sessions

---
+++ frontend/components/posts/posts.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class Posts extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+
+  render () {
+    return (
+      <div className='posts-feed'>
+        <h1 className='posts'>posts feed here</h1>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+        <br/>
+        <p>a</p>
+      </div>
+    );
+  }
+}
+
+export default Posts;
